44be875bd7097440708087158a2061d19178853a
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-08-27 10:43:22 +0900

    マップ追加

--- src/Main.js
+++ src/Main.js
@@ -131,6 +131,7 @@ function Main() {
           <div className="buy-items">
           <h3>購入方法</h3>
             <Buy />
+            <div class="googleMap"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3275.480803271069!2d137.65987095058665!3d34.819001980308116!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzTCsDQ5JzA4LjQiTiAxMzfCsDM5JzQzLjQiRQ!5e0!3m2!1sja!2sjp!4v1598491822681!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style={{border:'0'}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></div>
           </div>
           <div className='contact-container'>
             <h3>お問い合わせ</h3>
