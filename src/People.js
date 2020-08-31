edd75b1ed3b962086e25c9fd90ea49d044076cac
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-08-31 17:17:44 +0900

    写真追加

--- src/People.js
+++ src/People.js
@@ -4,7 +4,7 @@ class People extends React.Component {
   render() {
     return (
       <div className="loop_wrap">
-        <img src="dog-434834_640.jpg"></img><img src="farmer-657333_640.jpg"></img>
+        <img src="noukapics.jpg"></img><img src="noukapics.jpg"></img>
       </div>
     );
   }
